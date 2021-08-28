f0626c55aa5141f43f1d046e8c5eccdab1b6b481
Mingky Kim <45954551+mingkyme@users.noreply.github.com>
2021-08-28 13:40:18 +0900

    http https mode 추가

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const PORT = 3000;
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 
 const googleApp = require('./google/google');
 const clovaApp = require('./clova/clova');
 
 const app = express();
 const router = express.Router();
+// HTTPS Mode
+/*
+const https = require('https');
+const fs = require('fs');
 const options = {
     key: fs.readFileSync('../privkey.pem'),
     cert: fs.readFileSync('../cert.pem'),
     ca: fs.readFileSync('../fullchain.pem')
 };
+https.createServer(options, app).listen(PORT);
+*/
+
+// HTTP Mode
+app.listen(PORT);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/google', googleApp);
 app.use('/clova', clovaApp);
-
-https.createServer(options, app).listen(PORT);
